8f1b9cd19a7fe8f73a7abb4e086fc21cf2c3c210
Ferhat Çalışkan <ferhatclkn52@outlook.com>
2025-07-17 16:41:31 +0300

    Initial Commit Angular Frontend

---
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+
+import { AlertifyOptions, AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent extends BaseComponent implements OnInit {
+
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService) {
+    super(spinner)
+  }
+
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallPulse)
+  }
+  m() {
+    this.alertify.message("Merhaba", {
+      messageType: MessageType.Success,
+      position: Position.TopLeft,
+      delay: 5,
+    })
+  }
+  d() {
+    this.alertify.dismiss();
+  }
+
+}
---
+++ src/app/entitites/user.ts
@@ -0,0 +1,9 @@
+import { StickyDirection } from "@angular/cdk/table";
+
+export class User {
+    nameSurname: string;
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: StickyDirection
+}
