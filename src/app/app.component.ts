db208a75aeb87898106a7d8a0a5bb010335d70ff
Ferhat Çalışkan <ferhatclkn52@outlook.com>
2025-07-21 09:51:20 +0300

    Before Angular 14 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './services/common/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+import { Router } from '@angular/router';
 declare var $: any //jquery
 
 @Component({
@@ -11,13 +12,14 @@ declare var $: any //jquery
 })
 export class AppComponent {
 
-  constructor(public authService: AuthService, private toastrService: CustomToastrService) {
+  constructor(public authService: AuthService, private toastrService: CustomToastrService, private router: Router) {
     authService.identityCheck();
   }
 
   signOut() {
     localStorage.removeItem("accessToken");
     this.authService.identityCheck();
+    this.router.navigate(["login"])
     this.toastrService.message("Oturumunuz kapatılmıştır", "Oturum Kapatıldı", {
       messageType: ToastrMessageType.Info,
       position: ToastrPosition.TopRight
--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { StickyDirection } from '@angular/cdk/table';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AuthService } from 'src/app/services/common/auth.service';
@@ -12,7 +13,12 @@ import { UserService } from 'src/app/services/common/models/user.service';
 })
 export class LoginComponent extends BaseComponent implements OnInit {
 
-  constructor(private userService: UserService, spinner: NgxSpinnerService, private authService: AuthService) {
+  constructor(
+    private userService: UserService,
+    spinner: NgxSpinnerService, private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {
+
     super(spinner)
   }
 
@@ -23,6 +29,12 @@ export class LoginComponent extends BaseComponent implements OnInit {
     this.showSpinner(SpinnerType.BallSpinClockwise)
     await this.userService.login(usernameOrEmail, password, () => {
       this.authService.identityCheck();
+
+      this.activatedRoute.queryParams.subscribe(params => {
+        const returnUrl: string = params["returnUrl"];
+        if (returnUrl)
+          this.router.navigate([returnUrl]);
+      });
       this.hideSpinner(SpinnerType.BallSpinClockwise);
     })
   }
