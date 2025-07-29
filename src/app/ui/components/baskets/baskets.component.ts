29f26046da5b906c9dd0da0c77cdb21e0e4b2818
Ferhat Çalışkan <ferhatclkn52@outlook.com>
2025-07-29 14:56:48 +0300

    Çoklu SignalR Bağlantısını Sağlama ve Düzeltmeler

--- src/app/ui/components/baskets/baskets.component.ts
+++ src/app/ui/components/baskets/baskets.component.ts
@@ -5,6 +5,9 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Basket_Item } from 'src/app/contracts/basket/list_basket_item';
 import { Update_Basket_Item } from 'src/app/contracts/basket/update_basket_item';
 import { Create_Order } from 'src/app/contracts/order/create_order';
+import { BasketItemDeleteState, BasketItemRemoveDialogComponent } from 'src/app/dialogs/basket-item-remove-dialog/basket-item-remove-dialog.component';
+import { ShoppingCompleteDialogComponent, ShoppingCompleteState } from 'src/app/dialogs/shopping-complete-dialog/shopping-complete-dialog.component';
+import { DialogService } from 'src/app/services/common/dialog.service';
 import { BasketService } from 'src/app/services/common/models/basket.service';
 import { OrderService } from 'src/app/services/common/models/order.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
@@ -23,6 +26,7 @@ export class BasketsComponent extends BaseComponent implements OnInit {
     private basketService: BasketService,
     private orderService: OrderService,
     private toastrService: CustomToastrService,
+    private dialogService: DialogService,
     private router: Router) {
 
     super(spinner)
@@ -48,27 +52,43 @@ export class BasketsComponent extends BaseComponent implements OnInit {
   }
 
 
-  async removeBasketItem(basketItemId: string) {
-    this.showSpinner(SpinnerType.BallSpinClockwise);
-    await this.basketService.remove(basketItemId);
+  removeBasketItem(basketItemId: string) {
+    $("#basketModal").modal("hide");
 
-    var a = $("." + basketItemId)
-    $("." + basketItemId).fadeOut(500, () => this.hideSpinner(SpinnerType.BallSpinClockwise));
+
+    this.dialogService.openDialog({
+      componentType: BasketItemRemoveDialogComponent,
+      data: BasketItemDeleteState.Yes,
+      afterClosed: async () => {
+        this.showSpinner(SpinnerType.BallAtom);
+        await this.basketService.remove(basketItemId);
+
+        var a = $("." + basketItemId)
+        $("." + basketItemId).fadeOut(500, () => this.hideSpinner(SpinnerType.BallAtom));
+        $("#basketModal").modal("show");
+      }
+    });
   }
 
-  async shoppingComplete() {
-    this.showSpinner(SpinnerType.BallSpinClockwise)
+  shoppingComplete() {
+    $("#basketModal").modal("hide");
 
-    const order: Create_Order = new Create_Order();
-    order.address = "İstanbul";
-    order.description = "Deneme";
-    await this.orderService.create(order)
-
-    this.hideSpinner(SpinnerType.BallSpinClockwise)
-    this.toastrService.message("Sipariş Başarıyla Oluşturuldu.", "", {
-      messageType: ToastrMessageType.Info,
-      position: ToastrPosition.TopCenter
-    })
-    this.router.navigate(["/"]);
+    this.dialogService.openDialog({
+      componentType: ShoppingCompleteDialogComponent,
+      data: ShoppingCompleteState.Yes,
+      afterClosed: async () => {
+        this.showSpinner(SpinnerType.BallAtom);
+        const order: Create_Order = new Create_Order();
+        order.address = "Yenimahalle";
+        order.description = "Falanca filanca...";
+        await this.orderService.create(order);
+        this.hideSpinner(SpinnerType.BallAtom);
+        this.toastrService.message("Sipariş alınmıştır!", "Sipariş Oluşturuldu!", {
+          messageType: ToastrMessageType.Info,
+          position: ToastrPosition.TopRight
+        })
+        this.router.navigate(["/"]);
+      }
+    });
   }
 }
